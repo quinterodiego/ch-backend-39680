f1b0a9e66e887199161c0df9851f68a41a67541a
quinterodiego <d86webs@gmail.com>
2023-01-20 21:25:52 -0300

    desafio clase05

--- Product.Manager.js
+++ Product.Manager.js
@@ -1,40 +1,83 @@
+const fs = require('fs')
+
 class ProductManager {
-    constructor () {
-        this.products = []
+    constructor (pPath) {
+        this.path = pPath
     }
 
-    addProduct(product) {
-        if(this.products.length >= 0){
-            const code = this.products.find(p => p.code === product.code)
+    async nextID() {
+        const data = await fs.promises.readFile(this.path, 'utf-8')
+        const products = await JSON.parse(data)
+        if(products.length > 0){
+            const lastObject = products[products.length-1]
+            const id = lastObject.id + 1
+            return id
+        }else{
+            const id = 1
+            return id
+        }
+    }
+
+    async addProduct (product) {
+
+        const data = await fs.promises.readFile(this.path, 'utf-8')
+
+        const products = await JSON.parse(data);
+
+        if(products.length >= 0){
+            const code = products.find(p => p.code === product.code)
             if(code) {
                 console.log('Ya existe el codigo de producto')
                 return
             }
-            product.id = this.products.length + 1
-            this.products.push(product)
+            product.id = await this.nextID()
+            products.push(product)
+            await fs.promises.writeFile(this.path, JSON.stringify(products))
         } else {
             product.id = 0
-            this.products.push(product)
+            products.push(product)
         }
     }
 
-    getProducts() {
-        console.log(this.products)
+    async getProducts() {
+        const data = await fs.promises.readFile(this.path, 'utf-8')
+        const products = await JSON.parse(data)
+        console.log(products)
     }
 
-    getProductById(id) {
-        const product = this.products.find(p => p.id === id)
+    async getProductById(id) {
+        const data = await fs.promises.readFile(this.path, 'utf-8')
+        const products = await JSON.parse(data)
+        const product = products.find(p => p.id === id)
         if(product){
             console.log(product)
         } else {
             console.log('Not found')
         }
     }
+
+    async updateProduct(product) {
+        const data = await fs.promises.readFile(this.path, 'utf-8')
+        const products = await JSON.parse(data)
+        const productsToUpdate = products.filter(p => p.id !== product.id)
+        const newProducts = [...productsToUpdate, product]
+        newProducts.sort((a, b) => {
+            return a.id - b.id;
+        });
+        await fs.promises.writeFile(this.path, JSON.stringify(newProducts))
+    }
+
+    async deleteProduct(id) {
+        const data = await fs.promises.readFile(this.path, 'utf-8')
+        const products = await JSON.parse(data)
+        const newProducts = products.filter(p => p.id !== id)
+        await fs.promises.writeFile(this.path, JSON.stringify(newProducts))
+    }
 }
 
-const productManager = new ProductManager()
+const productManager = new ProductManager('./products.txt')
 
-productManager.getProducts()
+// productManager.getProducts()
 
 const product1 = {
     title: 'producto1',
@@ -63,13 +106,11 @@ const product3 = {
     stock: 50
 }
 
-productManager.addProduct(product1)
+// productManager.addProduct(product1)
 productManager.addProduct(product2)
-productManager.addProduct(product3)
+// productManager.addProduct(product3)
 
-console.log('|************************Listado de productos************************|')
-productManager.getProducts()
-
+// productManager.getProducts()
 
 const product4 = {
     title: 'producto4',
@@ -80,7 +121,24 @@ const product4 = {
     stock: 50
 }
 
-productManager.addProduct(product4)
+// productManager.addProduct(product4)
 
-console.log('|***********************Buscar producto*************************|')
-productManager.getProductById(3)
+// productManager.getProductById(2)
+
+const productToUpdate = {
+    title: 'producto2 actualizado',
+    description: 'ththththththth',
+    price: 5000,
+    thumbnail: 'htpp://yyyyyyyyyyyyy',
+    code: 'sdfskjdhf11111',
+    stock: 80,
+    id: 2
+}
+
+// productManager.updateProduct(productToUpdate)
+
+// productManager.getProducts()
+
+// productManager.deleteProduct(2)
+
+// productManager.getProducts()
